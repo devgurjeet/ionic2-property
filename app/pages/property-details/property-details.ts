5cafa1a25f283b8fb235ae4e37894b178fffb33c
Gurjeet Singh <developer.gurjeet@gmail.com>
2016-09-29 17:22:22 +0530

    Property details page done

--- app/pages/property-details/property-details.ts
+++ app/pages/property-details/property-details.ts
@@ -4,18 +4,18 @@ import {PropertyService} from '../../providers/property-service/property-service
 
 
 @Component({
-  templateUrl: 'build/pages/property-details/property-details.html',
+    templateUrl: 'build/pages/property-details/property-details.html',
+    providers: [PropertyService]
 })
 export class PropertyDetailsPage {
-   	property;
-
-   	static get parameters() {
+    property;
+    static get parameters() {
         return [[NavController], [NavParams], [PropertyService]];
     }
 
   	constructor(private navCtrl: NavController,navParams: NavParams, public propertyService: PropertyService) {
   		this.navCtrl         = navCtrl;
   		this.propertyService = propertyService;
-  		this.property        = navParams.get('property');
+  		this.property        = navParams.data.property;
   	}
 }
